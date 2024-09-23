51930cf4ca6f7453b296d817b989f3833338f85b
Lucas Oliveira <lucas.firmino@>
2024-09-23 12:18:22 -0300

    chore: first commit

---
+++ App.tsx
@@ -0,0 +1,156 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {
+  AnalyticsProvider,
+  createClient,
+  SegmentClient,
+  useAnalytics,
+} from '@segment/analytics-react-native';
+import {IdfaPlugin} from '@segment/analytics-react-native-plugin-idfa';
+import {AppsflyerPlugin} from '@segment/analytics-react-native-plugin-appsflyer';
+
+function segmentClientFn(): SegmentClient {
+  const client = createClient({
+    writeKey: 'SEGMENT_API_KEY',
+    trackAppLifecycleEvents: true,
+    flushInterval: 5,
+    collectDeviceId: true,
+    trackDeepLinks: true,
+    debug: true,
+  });
+
+  if (Platform.OS === 'ios') {
+    client.add({plugin: new IdfaPlugin()});
+  }
+
+  client.add({plugin: new AppsflyerPlugin()});
+
+  return client;
+}
+
+const segmentClient = segmentClientFn();
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const {track, identify} = useAnalytics();
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const WrapperApp = () => (
+  <AnalyticsProvider client={segmentClient}>
+    <App />
+  </AnalyticsProvider>
+);
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default WrapperApp;
